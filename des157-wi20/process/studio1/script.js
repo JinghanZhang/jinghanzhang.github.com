99438f399d923464741e2d66fb9e0786d9e57465
JinghanZhang <44076008+JinghanZhang@users.noreply.github.com>
2020-02-06 11:47:34 -0800

    studio1

---
+++ des157-wi20/process/studio1/script.js
@@ -0,0 +1,12 @@
+
+function showinput() {
+    var input1 = document.getElementById("userInput1").value;
+    var input2 = document.getElementById("userInput2").value;
+    var input3 = document.getElementById("userInput3").value;
+    var input4 = document.getElementById("userInput4").value;
+    var input5 = document.getElementById("userInput5").value;
+
+    document.getElementById("display1").innerHTML = input1;
+
+   // alert(`${input1}, ${input2}, ${input3}, ${input4}, ${input5}`);
+}
